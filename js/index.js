ea263ef71c16ba6ed870ff714df9db26cd51c4d4
Petermwansa <mwansapeter800@gmail.com>
2023-10-03 13:36:36 +0300

    add the a navbar but still have to make the navbar disappear when a link is clicked

--- js/index.js
+++ js/index.js
@@ -1,62 +1,51 @@
-// ====================================== the toggle icon ==============================
+
+// ==================== The code for the navbar ================ 
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const navLink = document.querySelector('.nav-links .link');
 
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
-    navbar.classList.toggle('active')
+    // to toggle the nav 
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        // to animate the links 
+        navLinks.forEach ((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .8}s`;
+            }
+        })
+        //the burger aniamation 
+        burger.classList.toggle('toggle');
+    })
+
+
+
+
 }
 
 
 
-// ====================================== scroll sections active link ==============================
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a ')
+// // ================================== for the scroll reveaL ==========================================
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
+// ScrollReveal({ 
+//     reset: false,
+//     distance: '80px',
+//     duration: 2000,
+//     delay: 200
+// });
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-            });
-        };
-    });
-
-// ================================== sticky navbar ==========================================
-    let header = document.querySelector('header');
-
-    header.classList.toggle('sticky', window.scrollY > 100)
-
-// ================================== remove toggle and navbar when link is clicked ==========================================
-
-    menuIcon.classList.remove('bx-x')
-    navbar.classList.remove('active')
-
-};
-
-
-// ================================== for the scroll reveaL ==========================================
-
-ScrollReveal({ 
-    reset: false,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-});
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 500 });
-ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img' , { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content' , { origin: 'right' });
+// ScrollReveal().reveal('.home-content, .heading', { origin: 500 });
+// ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box, .contact form', { origin: 'bottom' });
+// ScrollReveal().reveal('.home-content h1, .about-img' , { origin: 'left' });
+// ScrollReveal().reveal('.home-content p, .about-content' , { origin: 'right' });
 
 
 // ================================== typed js ==========================================
@@ -86,4 +75,6 @@ upwards = () => {
 }
 
 
+
 upwards();
+navSlide();
