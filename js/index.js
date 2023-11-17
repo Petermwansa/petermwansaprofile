3c7bdab1b4f62bfa7ffb6b97aee4331ed844493d
Petermwansa <mwansapeter800@gmail.com>
2023-11-17 21:34:21 +0300

    Add a the responsiveness to the texts

--- js/index.js
+++ js/index.js
@@ -41,6 +41,7 @@ const navSlide = () => {
         }
         
         nav.classList.toggle("nav-active");
+        burger.classList.toggle("toggle");
 
     }
 
@@ -169,7 +170,7 @@ const animator = () => {
 // ================================== typed js ==========================================
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Web Developer', 'UI/UX Designer'],
+    strings: ['Web Developer', 'iOS Developer', 'UI/UX Designer'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
